bacca2801972f9e880e81abb245ff852e4deeb83
Daniel Kamenov <43839211+deekaayy@users.noreply.github.com>
2020-11-28 20:15:13 +0200

    Add files via upload

---
+++ summer-hmwrk/SD_SummerHW.cpp
@@ -0,0 +1,72 @@
+﻿#include "pch.h"
+#include "CLL.h"
+
+void doTests() {
+
+	std::cout << "Running test data... \n\n";
+
+	CircularLinkedList ll;
+
+	ll.addAtEnd("Street");
+	ll.addAtEnd("Taxi");
+	ll.addAtEnd("Ink");
+	ll.addAtEnd("Kongo");
+	ll.addAtEnd("Dog");
+	ll.addAtEnd("Smile");
+	ll.addAtEnd("Eat");
+	ll.addAtEnd("Tall");
+	ll.addAtEnd("Ker");
+	ll.addAtEnd("Rer");
+	ll.addAtEnd("Pass");
+	ll.addAtEnd("Sap");
+	///////////////////////////
+	ll.print();
+	ll.concatenateAllAvaiable();
+	ll.print();
+
+	///////////////////////////
+	ll.sort();
+	std::cout << "\nPost-sort...\n";
+	ll.print();
+
+	//////////////////////////////////
+	std::cout << "The biggest element is...\n"
+			  << ll.findLongest() << "\n\n";
+}
+
+void mainFunc() {
+	std::cout << "Enter as many elements as you wish, if you want to stop, type \"exit\"...\n";
+
+	CircularLinkedList ll;
+	std::string command;
+	while (true)
+	{
+		std::cin >> command;
+		if (command == "exit")
+		{
+			break;
+		}
+		ll.addAtEnd(command);
+	}
+
+	ll.print();
+	ll.concatenateAllAvaiable();
+	std::cout << "\nAfter the concatenation...\n";
+	ll.print();
+
+	ll.sort();
+	std::cout << "\nPost-sort...\n";
+	ll.print();
+
+	std::cout << "The biggest element is...\n"
+			  << ll.findLongest() << "\n\n";
+}
+
+int main()
+{
+	//doTests();
+
+	mainFunc();
+
+	return 0;
+}
